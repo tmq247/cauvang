2b2ae6de9410744ad56c9de803a0fbab8306ef06
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-02 21:07:00 +0700

    Update maintenance.py

--- SANKIXD/plugins/sudo/maintenance.py
+++ SANKIXD/plugins/sudo/maintenance.py
@@ -23,13 +23,13 @@ async def maintenance(client, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+    if state == "on":
         if await is_maintenance() is False:
             await message.reply_text(_["maint_4"])
         else:
             await maintenance_on()
             await message.reply_text(_["maint_2"].format(app.mention))
-    elif state == "disable":
+    elif state == "off":
         if await is_maintenance() is False:
             await maintenance_off()
             await message.reply_text(_["maint_3"].format(app.mention))
