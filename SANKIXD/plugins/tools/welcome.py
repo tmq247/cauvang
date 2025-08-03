a6a2803ae098db8cdb60848733b97995492df708
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-03 18:48:51 +0700

    Update welcome.py

--- SANKIXD/plugins/tools/welcome.py
+++ SANKIXD/plugins/tools/welcome.py
@@ -6,6 +6,7 @@ from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
 from SANKIXD import app
+from SANKIXD.utils.dbfeds import check_banned_user, get_fed_id
 
 LOGGER = getLogger(__name__)
 
@@ -33,37 +34,11 @@ class temp:
     U_NAME = None
     B_NAME = None
 
-def circle(pfp, size=(500, 500)):
-    pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
-    bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
-    mask = Image.new("L", bigsize, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0) + bigsize, fill=255)
-    mask = mask.resize(pfp.size, Image.LANCZOS)
-    mask = ImageChops.darker(mask, pfp.split()[-1])
-    pfp.putalpha(mask)
-    return pfp
 
 
-def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("SANKIXD/assets/tsanki.png")
-    pfp = Image.open(pic).convert("RGBA")
-    pfp = circle(pfp)
-    pfp = pfp.resize((1157, 1158))
-    draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('SANKIXD/assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('SANKIXD/assets/font.ttf', size=60)
-    draw.text((1800, 700), f'NAME: {user}', fill=(255, 255, 255), font=font)
-    draw.text((1800, 830), f'ID: {id}', fill=(255, 255, 255), font=font)
-    draw.text((1800, 965), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
-    pfp_position = (391, 336)
-    background.paste(pfp, pfp_position, pfp)
-    background.save(f"downloads/welcome#{id}.png")
-    return f"downloads/welcome#{id}.png"
-
-#@app.on_chat_member_updated(filters.group, group=-3)
+@app.on_chat_member_updated(filters.group, group=-3)
 async def greet_group(_, member: ChatMemberUpdated):
-    chat_id = member.chat.id
+    chat = member.chat.id
     A = await wlcm.find_one(chat_id)
     if (
         not member.new_chat_member
@@ -73,40 +48,26 @@ async def greet_group(_, member: ChatMemberUpdated):
         return
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
     try:
-        pic = await app.download_media(
-            user.photo.big_file_id, file_name=f"pp{user.id}.png"
-        )
-    except AttributeError:
-        pic = "SANKIXD/assets/tsanki.png"
+        fed_id = await get_fed_id(chat.id)
+        if fed_id:
+            check_user = await check_banned_user(fed_id, member.id)
+            if check_user:
+                reason = check_user["reason"]
+                date = check_user["date"]
+                await chat.ban_member(member.id)
+                return await app.send_message(
+                    chat.id,
+                    f"**Người dùng {member.mention} đã bị fban.\n\nLý do: {reason}.\nNgày: {date}.**",
+                )
+    except ChatAdminRequired:
+        return
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
         except Exception as e:
             LOGGER.error(e)
-    try:
-        welcomeimg = welcomepic(
-            pic, user.first_name, member.chat.title, user.id, user.username
-        )
-        temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
-            member.chat.id,
-            photo=welcomeimg,
-            caption=f"""
-Wᴇʟᴄᴏᴍᴇ Tᴏ {member.chat.title}
-╭───── • ◆ • ─────╮
-  Nᴀᴍᴇ ✧ {user.mention}
-  Iᴅ ✧ {user.id}
-  Usᴇʀɴᴀᴍᴇ ✧ @{user.username}
-╰───── • ◆ • ─────╯
-""",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/muoimuoiamnhac_Bot?startgroup=true")]])
-        )
-    except Exception as e:
-        LOGGER.error(e)
-    try:
-        os.remove(f"downloads/welcome#{user.id}.png")
-        os.remove(f"downloads/pp{user.id}.png")
-    except Exception as e:
-        pass
+    
+    
 
 @app.on_message(filters.new_chat_members & filters.group, group=-1)
 async def bot_wel(_, message):
