ddf20a39d1fd3fdd1265d5acc05283dc8188416a
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-04 15:52:23 +0700

    Update functions.py

--- SANKIXD/utils/functions.py
+++ SANKIXD/utils/functions.py
@@ -40,7 +40,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from pyrogram import utils
 
-#from SANKIXD import aiohttpsession as aiosession
+from SANKIXD import aiohttpsession as aiosession
 #from wbb.utils.dbfunctions import start_restart_stage
 from SANKIXD.utils.http import get, post
 
