f94c44dbbc448046e15247a1174d725bef2283eb
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-06 23:20:24 +0700

    Update feds.py

--- SANKIXD/plugins/tools/feds.py
+++ SANKIXD/plugins/tools/feds.py
@@ -347,14 +347,14 @@ async def join_fed(client, message):
     else:
         if member.status == ChatMemberStatus.OWNER or ChatMemberStatus.ADMINISTRATOR:
             required_permissions = [
-            member.can_change_info,
-            member.can_delete_messages,
-            member.can_invite_users,
-            member.can_restrict_members,
-            member.can_pin_messages,
-            member.can_promote_members,
-            member.can_manage_chat,
-            member.can_manage_video_chats,
+            member.privileges.can_change_info,
+            member.privileges.can_delete_messages,
+            member.privileges.can_invite_users,
+            member.privileges.can_restrict_members,
+            member.privileges.can_pin_messages,
+            member.privileges.can_promote_members,
+            member.privileges.can_manage_chat,
+            member.privileges.can_manage_video_chats,
         ]
             if not all(required_permissions):
                 return await message.reply_text("Bạn cần có tất cả các quyền quản trị viên trong nhóm để sử dụng lệnh này!")
