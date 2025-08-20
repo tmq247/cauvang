765b90a96f312ad01b4c564e4b79dbaf06afebd8
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-21 01:35:57 +0700

    Update feds.py

--- SANKIXD/plugins/tools/feds.py
+++ SANKIXD/plugins/tools/feds.py
@@ -836,7 +836,7 @@ async def fdel(client, message):
                     if served_chat != chat.id:
                         if not message.text.startswith("/s"):
                             await app.send_message(served_chat, f"**Bị xóa tin nhắn trong liên đoàn :{user.mention} !**")
-                await userbot.delete_user_history(served_chat, user.id)
+                await ubot.delete_user_history(served_chat, user.id)
                 number_of_chats += 1
             await asyncio.sleep(1)
         except FloodWait as e:
