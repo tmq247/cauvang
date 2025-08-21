0aeba5186483a19cfcd7eed5065462b2649580d8
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-21 21:06:48 +0700

    Update feds.py

--- SANKIXD/plugins/tools/feds.py
+++ SANKIXD/plugins/tools/feds.py
@@ -823,12 +823,12 @@ async def fdel(client, message):
     if 1 in assistants:
         ubot = us.one
     app2 = await ubot.get_me()
-    user1 = await app2.get_users(user_id)
+    user1 = await app2.get_users(user.id)
     for served_chat in served_chats:
         try:
             try:
                 chat_member = await app.get_chat_member(served_chat, user.id)
-                chat_member = await ubot.get_chat_member(served_chat, user.id)
+                chat_member = await app2.get_chat_member(served_chat, user1.id)
             except UserNotParticipant:
                 continue
             #if chat_member.status == ChatMemberStatus.MEMBER:
@@ -838,7 +838,7 @@ async def fdel(client, message):
             checkadmin = admin.privileges.can_delete_messages
             if not checkadmin:
                 return await app.send_message(served_chat, "userbot không có quyền xóa tin nhắn người dùng này.")
-            await app2.delete_user_history(served_chat, user.id)
+            await app2.delete_user_history(served_chat, user1.id)
             number_of_chats += 1
             await asyncio.sleep(1)
         except FloodWait as e:
