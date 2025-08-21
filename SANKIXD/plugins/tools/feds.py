4af2db520776579246eafc7d3cb29599c53f8a83
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-21 21:03:16 +0700

    Update feds.py

--- SANKIXD/plugins/tools/feds.py
+++ SANKIXD/plugins/tools/feds.py
@@ -822,8 +822,8 @@ async def fdel(client, message):
     number_of_chats = 0
     if 1 in assistants:
         ubot = us.one
-        
     app2 = await ubot.get_me()
+    user1 = await app2.get_users(user_id)
     for served_chat in served_chats:
         try:
             try:
