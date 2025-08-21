97c9b53ffa8f4447f7d7f1592c2b0c4b9b308fb8
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-08-21 20:40:26 +0700

    Update feds.py

--- SANKIXD/plugins/tools/feds.py
+++ SANKIXD/plugins/tools/feds.py
@@ -832,19 +832,18 @@ async def fdel(client, message):
                 continue
             #if chat_member.status == ChatMemberStatus.MEMBER:
             admin = await app.get_chat_member(served_chat, app2.id)
-            print(admin)
-            if admin.status == ChatMemberStatus.ADMINISTRATOR:
-                continue
+            if admin.status != ChatMemberStatus.ADMINISTRATOR:
+                return await app.send_message("userbot không có quyền qtv trong nhóm này.")
             checkadmin = admin.privileges.can_delete_messages
             if not checkadmin:
-                return await message.reply_text("userbot không có quyền xóa tin nhắn người dùng này.")
+                return await app.send_message("userbot không có quyền xóa tin nhắn người dùng này.")
             await ubot.delete_user_history(served_chat, user.id)
             number_of_chats += 1
             await asyncio.sleep(1)
         except FloodWait as e:
             await asyncio.sleep(int(e.value))
         except Exception:
-            return
+            return app.send_message("Lỗi")
     await m.edit(f"Đã xóa tin nhắn trong liên đoàn: {user.mention} !")
     try:
         await m.edit(
